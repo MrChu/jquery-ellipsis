354b757aae986791e4fd3e8225d53d1f71fde8f3
褚为标 <597989975@qq.com>
2015-10-08 19:20:29 +0800

    Initial Rrelease

---
+++ jquery-ellipsis-1.0.js
@@ -0,0 +1,25 @@
+﻿/*
+ * File:        jquery.ellipsis.js
+ * Version:     1.0
+ * Author:      MrChu
+ * Info:        www.chuweibiao.com
+ * Smile to life whatever and whenever.
+ */
+(function ($) {
+	$.fn.ellipsis = function (options) {
+		var hoverTip = true;
+		var openTip = options.openTip;
+		if (typeof(openTip) != "undefined" && !openTip)
+			hoverTip = false;
+		var limitLength = options.maxLength;
+		this.each(function () {
+			var originalText = this.innerText;
+			if (originalText.length > limitLength) {
+				if (hoverTip)
+					this.title = originalText;
+				originalText = originalText.substr(0, limitLength);
+				this.innerText = originalText + "...";
+			}
+		});
+	};
+})(jQuery);
